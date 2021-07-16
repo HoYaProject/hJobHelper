29b9061a3023884ebe44ef4802bbfee6c8c19d12
llHoYall <hoya128@gmail.com>
2021-07-16 21:59:45 +0900

    Resolved Issue #1
    Create common header and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <h4 class="mt-4 text-sm text-gray-700">&copy; 2021 HoYa</h4>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Footer",
+});
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
+  <Header title="About" />
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <Footer />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default defineComponent({
+  name: "About",
+  components: {
+    Header,
+    Footer,
+  },
+});
+</script>
